e3f0b6439cf8fe0fa21347c08fa1c48aeee06802
Yoshiya Hinosawa <stibium121@gmail.com>
2020-03-26 01:37:00 +0900

    feat: update lepont and type

--- src/bridge.ts
+++ src/bridge.ts
@@ -1,4 +1,4 @@
-import { useBridge } from 'lepont'
+import { useBridge, Bridge, Registry } from 'lepont'
 import {
   MSG_TYPE_SET_ITEM,
   MSG_TYPE_GET_ITEM,
@@ -7,18 +7,18 @@ import {
 } from './shared'
 import AsyncStorage from '@react-native-community/async-storage'
 
-export function useAsyncStorage(registry: any) {
+export function useAsyncStorage(registry: Registry) {
   useBridge(
     registry,
     MSG_TYPE_SET_ITEM,
-    async (p: SetPayload, _: unknown): Promise<void> => {
+    async (p: SetPayload, _: Bridge): Promise<void> => {
       await AsyncStorage.setItem(p.key, p.value)
     }
   )
   useBridge(
     registry,
     MSG_TYPE_GET_ITEM,
-    (p: GetPayload, _: unknown): unknown => {
+    (p: GetPayload, _: Bridge): Promise<string | null> => {
       return AsyncStorage.getItem(p.key)
     }
   )
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { MSG_TYPE_GET_ITEM, MSG_TYPE_SET_ITEM } from './shared'
  * Sets the item to the async storage.
  */
 export async function setItem(key: string, value: unknown): Promise<void> {
-  await sendMessage({
+  await sendMessage<void, any>({
     type: MSG_TYPE_SET_ITEM,
     payload: {
       key,
@@ -18,7 +18,7 @@ export async function setItem(key: string, value: unknown): Promise<void> {
  * Gets the item from the async storage.
  */
 export async function getItem(key: string): Promise<any> {
-  const json = await sendMessage<string>({
+  const json = await sendMessage<string, { key: string }>({
     type: MSG_TYPE_GET_ITEM,
     payload: { key },
   })
